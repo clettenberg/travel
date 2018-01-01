4f3a83439f8484d55f6c7fa4802449557f1d98af
Chase Clettenberg <clettenberg@gmail.com>
2018-01-01 11:33:42 -0600

    Add points of interest to trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :pois
   root to: redirect("/trips")
+  resources :pois
   resources :trips
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
