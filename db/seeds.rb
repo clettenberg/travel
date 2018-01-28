4196fc3d4eefbe5c2cac56525d2405547235d9ee
Chase Clettenberg <clettenberg@gmail.com>
2018-01-28 13:27:38 -0600

    Add date to trips views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@ user = User.new(
 user.skip_confirmation!
 user.save!
 
-scotland = user.trips.create(title: "Scotland")
+scotland = user.trips.create(title: "Scotland", start_date: Date.today, end_date: Date.today + 7.days)
 pois = ["The Elephant House", "Arthur's Seat", "Castle", "National Musuem"].map { |poi| { title: poi } }
 scotland.pois.create(pois)
 
 
-south_africa = user.trips.create(title: "South Africa")
+south_africa = user.trips.create(title: "South Africa", start_date: Date.today, end_date: Date.today + 30.days)
 pois = ["Table Mountain", "Hout Bay", "City Centre", "Robben Island"].map { |poi| { title: poi } }
 south_africa.pois.create(pois)
