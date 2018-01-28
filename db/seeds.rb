32a7dca301dbf3de3758852e13233ff165ea8cb3
Chase Clettenberg <clettenberg@gmail.com>
2018-01-27 22:35:49 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(
+  email:                 "user@mail.com",
+  password:              "password",
+  password_confirmation: "password"
+)
+user.skip_confirmation!
+user.save!
+
+scotland = user.trips.create(title: "Scotland")
+pois = ["The Elephant House", "Arthur's Seat", "Castle", "National Musuem"].map { |poi| { title: poi } }
+scotland.pois.create(pois)
+
+
+south_africa = user.trips.create(title: "South Africa")
+pois = ["Table Mountain", "Hout Bay", "City Centre", "Robben Island"].map { |poi| { title: poi } }
+south_africa.pois.create(pois)
