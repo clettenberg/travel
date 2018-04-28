ca6c6f50128cf70b5c4abbbb580af5afe3e9c79e
Chase Clettenberg <clettenberg@gmail.com>
2018-04-28 11:50:46 -0500

    Make sure trip exists for spec

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe PlacesController, type: :controller do
 
   describe "#show" do
-    let(:place) { FactoryBot.create(:place, place_id: 'ChIJj9dEC-9YdhwRTDw4wzsEnt4', trip_id: 1) }
+    let(:trip) { FactoryBot.create(:trip_with_places, places_count: 1)}
 
     it 'should return json details' do
+      place = trip.places.first
+      place.place_id = 'ChIJj9dEC-9YdhwRTDw4wzsEnt4'
+      place.save
       VCR.use_cassette("GooglePlaceService/get_details/given_a_valid_place_id/should_return_the_place_details") do
         get :show, params: { id: place.id }, format: :json
         expect(response.content_type).to eq "application/json"
