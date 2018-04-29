780136572de29af060b94531110a43eac7dcf5ae
Chase Clettenberg <clettenberg@gmail.com>
2018-04-29 16:32:01 -0500

    Memoize details and display them on place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,16 @@
 class PlacesController < ApplicationController
   def show
-    place = Place.find(place_params[:id])
-    @details = GooglePlaceService.get_details(place.place_id)
-
+    @place = Place.find(place_params[:id])
+    @details = details
     respond_to do |format|
       format.html
       format.json { render json: @details }
     end
+  end
 
+  def details
+    return @details if defined?(@details)
+    @details = GooglePlaceService.get_details(@place.place_id)
   end
 
   private
