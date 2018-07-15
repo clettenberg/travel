9898fdd50de58870e2e29656e0bee9cf5dc05ab2
Chase Clettenberg <clettenberg@gmail.com>
2018-07-14 21:50:19 -0500

    Add reverse nominatim lookup

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,7 +1,41 @@
 class MapquestService
+  MAPQUEST_NOMINATIM_ROOT_URL = "https://open.mapquestapi.com/nominatim/v1"
+
   def search(query)
-    url = "https://open.mapquestapi.com/nominatim/v1/search.php?key=#{ENV['MAPQUEST_API_KEY']}&format=json&q=#{query}&addressdetails=1"
+    params = default_params.merge({
+      q: query,
+      addressdetails: 1
+    })
+
+    send_request("#{MAPQUEST_NOMINATIM_ROOT_URL}/search.php?#{params.to_query}")
+  end
+
+  def reverse(osm_id:, osm_type:)
+    osm_type_code = osm_type.first.capitalize
+    params = default_params.merge({
+      osm_type: osm_type.first.capitalize,
+      osm_id: osm_id
+    })
+
+    send_request("#{MAPQUEST_NOMINATIM_ROOT_URL}/reverse.php?#{params.to_query}")
+  end
+
+  private
+
+  def send_request(url)
     response = Faraday.get(url)
-    JSON.parse(response.body).map(&:deep_symbolize_keys)
+    json_body = JSON.parse(response.body)
+    if json_body.kind_of?(Array)
+      json_body.map(&:deep_symbolize_keys)
+    else
+      json_body.deep_symbolize_keys
+    end
+  end
+
+  def default_params
+    {
+      key: ENV['MAPQUEST_API_KEY'],
+      format: 'json'
+    }
   end
 end
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe MapquestService do
   describe 'search', :vcr do
-    let (:subject) { described_class.new }
+    let(:subject) { described_class.new }
     context 'returns results' do
       let(:results) { subject.search('Tower Grove Park') }
 
@@ -21,6 +21,43 @@ describe MapquestService do
                                                       :importance,
                                                       :address)
 
+      end
+    end
+  end
+
+  describe 'reverse', :vcr do
+    let(:subject) { described_class.new }
+    context 'with a valid osm place ID and osm type' do
+      it 'should return info' do
+        result = subject.reverse(osm_id: 26996903, osm_type: 'way')
+        expect(result).to include(
+          {
+            place_id: "78354868",
+            licence: "Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright",
+            osm_type: "way",
+            osm_id: "26996903",
+            lat: "51.5217805",
+            lon: "-0.162824059130876",
+            display_name: "The Landmark, Melcombe Place, Marylebone, City of Westminster, London, Greater London, England, NW1 6JR, UK",
+            address: {
+              hotel: "The Landmark",
+              road: "Melcombe Place",
+              suburb: "Marylebone",
+              city: "London",
+              state_district: "Greater London",
+              state: "England",
+              postcode: "NW1 6JR",
+              country: "UK",
+              country_code: "gb"
+            },
+            boundingbox: [
+              "51.5213853",
+              "51.5222046",
+              "-0.1635263",
+              "-0.1621871"
+            ]
+          }
+        )
       end
     end
   end
