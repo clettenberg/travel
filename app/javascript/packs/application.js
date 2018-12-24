01cc85f4e3586bc7dedd2be53d1cb0d604f62dfd
Chase Clettenberg <clettenberg@gmail.com>
2018-12-23 21:47:23 -0600

    Add javascript back in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-require("babel-polyfill");
+require('babel-polyfill');
+
 const componentRequireContext = require.context('components', true);
 const ReactRailsUJS = require('react_ujs');
 
