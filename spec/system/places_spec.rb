5d1aab11952ae435aac76878cd479e84e768a8aa
Chase Clettenberg <clettenberg@gmail.com>
2018-12-24 08:58:57 -0600

    Add spec for lat/lon search

--- spec/system/places_spec.rb
+++ spec/system/places_spec.rb
@@ -17,12 +17,29 @@ RSpec.describe 'places', type: :system do
       visit "/trips/#{trip.id}/places/new"
 
       fill_in 'Place Name', with: "Busch Stadium St Louis\n"
-      # expect(page).to have_css('.mapquest-search-results')
 
       find("label", text: "Busch Stadium, South 8th Street").click
+
+      fill_in 'Name', with: 'Cardinals Game'
+      find('trix-editor').click.set('Cards won!')
+
       click_on "Submit"
 
       expect(page).to have_content("Busch Stadium")
+    end
+
+    it 'allows a user to add a new place by lat lon', :vcr do
+      visit "/trips/#{trip.id}/places/new"
+      click_on 'Lat/Lon'
+      fill_in 'Latitude', with: '40.6892474'
+      fill_in 'Longitude', with: "-74.0445405280149\n"
+      find("label", text: "Statue of Liberty, Flagpole Plaza").click
+      fill_in 'Name', with: 'Day at the Statue of Liberty'
+      find('trix-editor').click.set('Great weather!')
+
+      click_on "Submit"
+
+      expect(page).to have_content("Statue of Liberty")
     end
   end
 end
