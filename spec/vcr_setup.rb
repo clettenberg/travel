8c72d11e9da509d09c06a08e99252d6f98d884d7
Chase Clettenberg <clettenberg@gmail.com>
2020-05-10 15:22:33 -0500

    Whitelist webdriver hosts

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,4 +1,7 @@
 require "vcr"
+require "webdrivers"
+
+driver_hosts = (ObjectSpace.each_object(Webdrivers::Common.singleton_class).to_a - [Webdrivers::Common]).map { |driver| URI(driver.base_url).host }
 
 VCR.configure do |c|
   c.cassette_library_dir = "spec/cassettes"
@@ -7,4 +10,5 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.allow_http_connections_when_no_cassette = true
   c.filter_sensitive_data("<API_KEY>") { ENV["MAPQUEST_API_KEY"] }
+  c.ignore_hosts(*driver_hosts)
 end
